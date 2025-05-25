6dff60e13f6e47ffd687135146369cd6c0732460
Aver Varian Hernawan <avervarianhernawan6@gmail.com>
2025-05-25 13:51:53 +0700

    feat(auth): Enhance login and registration flow with retry and back options

--- lib/controllers/auth_controller.go
+++ lib/controllers/auth_controller.go
@@ -31,15 +31,24 @@ func NewAuthController(service services.AuthService) *AuthController {
 // Parameters:
 //   - user: A pointer to a User model containing login credentials
 func (c *AuthController) Login(user *model.User) {
-	err := c.authService.Login(user)
-	if err != nil {
-		color.Red(err.Error())
-		fmt.Scanln()
-		return
-	}
+	for {
+		err := c.authService.Login(user)
+		if err != nil {
+			if err.Error() == "back" {
+				break
+			}
 
-	color.Green("Login successful! Welcome, %s!", user.Username)
-	fmt.Scanln()
+			if err.Error() == "continue" {
+				continue
+			}
+
+			color.Red(err.Error())
+			fmt.Scanln()
+			continue
+		} else {
+			break
+		}
+	}
 }
 
 // Register handles the user registration process.
@@ -48,13 +57,34 @@ func (c *AuthController) Login(user *model.User) {
 // Returns:
 //   - None, but prompts for user input and handles errors internally
 func (c *AuthController) Register() {
-	err := c.authService.Register()
-	if err != nil {
-		color.Red(err.Error())
-		fmt.Scanln()
-		return
-	}
+	//err := c.authService.Register()
+	//if err != nil {
+	//	color.Red(err.Error())
+	//	fmt.Scanln()
+	//	return
+	//}
+	//
+	//color.Green("Registration successful! Please login to continue.")
+	//fmt.Scanln()
 
-	color.Green("Registration successful! Please login to continue.")
-	fmt.Scanln()
+	for {
+		err := c.authService.Register()
+		if err != nil {
+			if err.Error() == "back" {
+				break
+			}
+
+			if err.Error() == "continue" {
+				continue
+			}
+
+			color.Red(err.Error())
+			fmt.Scanln()
+			continue
+		} else {
+			color.Green("Registration successful! Please login to continue.")
+			fmt.Scanln()
+			break
+		}
+	}
 }
